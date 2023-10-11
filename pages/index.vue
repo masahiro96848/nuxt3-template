ccf3762cef824a5bc2ac510a5825a4035f954143
masahiro96848 <feetwcpic9436@gmail.com>
2023-10-11 22:08:55 +0900

    vuetifyの環境構築

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>テスト</p>
+    <p>{{ count }}</p>
+    <v-btn @click="increment">add</v-btn>
+  </div>
+</template>
+
+<script setup lang="ts">
+const count = ref(0)
+
+const increment = () => {
+  count.value++
+}
+</script>
+
+<style scoped></style>
